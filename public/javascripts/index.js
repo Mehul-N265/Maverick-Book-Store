70784db305204de180ea7fd02b54381fc4d2de3c
Mehul-N265 <mehulnimbalkar265@gmail.com>
2021-09-09 20:42:32 +0530

    Check styles

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,10 +1,36 @@
 //settings btn\
+const menuBar = document.querySelector(".hidden-menu-bar");
+const menuBtn = document.querySelector(".menu-btn");
+const settingsBtn = document.querySelector(".settings");
+const accessoriesMenu = document.querySelector(".hidden-menu");
+const closeBtn = document.querySelector('#close-nav');
+const greeting = document.querySelector('#greet');
+showAccessories();
+showMenuBar();
+greetUser()
 
 function showAccessories() {
-    const settingsBtn = document.querySelector(".settings");
-    const accessoriesMenu = document.querySelector('.hidden-menu')
-    settingsBtn.addEventListener('click',() =>{
-        accessoriesMenu.classList.toggle('show-menu')
-    })
+ 
+  settingsBtn.addEventListener("click", () => {
+    accessoriesMenu.classList.toggle("show-menu");
+  });
 }
-showAccessories();
+function showMenuBar() {
+ 
+  menuBtn.addEventListener("click", () => {
+    menuBar.classList.toggle("menu-bar");
+  });
+  closeBtn.addEventListener('click',() =>{
+    menuBar.classList.toggle('menu-bar')
+  })
+}
+function greetUser(){
+  let date = new Date().getHours();
+  if(date >= 0 && date < 12){
+    greeting.textContent = "Good Morning";
+  }else if(date >=12 && date < 18){
+    greeting.textContent = "Good afternoon";
+  }else if(date >=18 && date <= 23){
+    greeting.textContent = "Good evening";
+  }
+}
