a1b08f7c5d1a1d6984c0ab71d476ca17b083c006
Mehul-N265 <mehulnimbalkar265@gmail.com>
2021-09-13 20:57:53 +0530

    Completed the index page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,34 +3,44 @@ const menuBar = document.querySelector(".hidden-menu-bar");
 const menuBtn = document.querySelector(".menu-btn");
 const settingsBtn = document.querySelector(".settings");
 const accessoriesMenu = document.querySelector(".hidden-menu");
-const closeBtn = document.querySelector('#close-nav');
-const greeting = document.querySelector('#greet');
+const closeBtn = document.querySelector("#close-nav");
+const greeting = document.querySelector("#greet");
+const quotesText = document.querySelector("#quotes-text");
+const quoteBy = document.querySelector("#quote-by");
+
 showAccessories();
 showMenuBar();
-greetUser()
-
+greetUser();
+randomQuote();
 function showAccessories() {
- 
   settingsBtn.addEventListener("click", () => {
     accessoriesMenu.classList.toggle("show-menu");
   });
 }
 function showMenuBar() {
- 
   menuBtn.addEventListener("click", () => {
     menuBar.classList.toggle("menu-bar");
   });
-  closeBtn.addEventListener('click',() =>{
-    menuBar.classList.toggle('menu-bar')
-  })
+  closeBtn.addEventListener("click", () => {
+    menuBar.classList.toggle("menu-bar");
+  });
 }
-function greetUser(){
+function greetUser() {
   let date = new Date().getHours();
-  if(date >= 0 && date < 12){
+  if (date >= 0 && date < 12) {
     greeting.textContent = "Good Morning";
-  }else if(date >=12 && date < 18){
+  } else if (date >= 12 && date < 18) {
     greeting.textContent = "Good afternoon";
-  }else if(date >=18 && date <= 23){
+  } else if (date >= 18 && date <= 23) {
     greeting.textContent = "Good evening";
   }
+}
+function randomQuote() {
+  fetch("https://api.quotable.io/random")
+    .then((res) => res.json())
+    .then((data) => {
+      quotesText.textContent = `"${data.content}"`;
+      quoteBy.textContent = `--${data.author}`;
+      console.log(`Grand output : ${data.content} --${data.author}`);
+    });
 }
